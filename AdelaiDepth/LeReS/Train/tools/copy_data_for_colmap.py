7ddc47d13a2d2b333e73bd4b02f300f97ee40d6c
mikacuy <muy@adobe.com>
2023-01-14 16:38:44 -0800

    setting up files for colmap

---
+++ AdelaiDepth/LeReS/Train/tools/copy_data_for_colmap.py
@@ -0,0 +1,35 @@
+import sys, os
+import datetime
+
+from functools import partial
+from multiprocessing.dummy import Pool
+from subprocess import call
+
+DUMP_DIR = "/orion/downloads/coordinate_mvs/taskonomy/tmp/rgbs"
+os.makedirs(DUMP_DIR, exist_ok=True)
+
+DATA_DIR = "/orion/downloads/coordinate_mvs/taskonomy/rgbs"
+
+scenes = os.listdir(DATA_DIR)
+
+print(scenes)
+print(len(scenes))
+
+
+commands = []
+for scene in scenes:
+	src_path = os.path.join(DATA_DIR, scene)
+
+	c = "cp -r " + src_path + " " + DUMP_DIR
+	commands.append(c)
+
+print("Copying scenes for colmap tmp")
+print("Number of commands in parallel: "+str(len(commands)))
+
+report_step = 24
+pool = Pool(report_step)
+for idx, return_code in enumerate(pool.imap(partial(call, shell=True), commands)):
+  if idx % report_step == 0:
+     print('[%s] command %d of %d' % (datetime.datetime.now().time(), idx, len(commands)))
+  if return_code != 0:
+     print('!! command %d of %d (\"%s\") failed' % (idx, len(commands), commands[idx]))
