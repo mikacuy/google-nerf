49ee41a2f650f7ca867f6ab2d1eb8f3e61f38b96
mikacuy <muy@adobe.com>
2023-05-03 00:48:06 -0700

    added nerf pytorch reimplementation

--- piecewise_linear/eval_all_dists7.py
+++ piecewise_linear/eval_all_dists7.py
@@ -2,12 +2,38 @@ import os, sys
 
 all_test_dist = [0.5, 0.75, 1.0]
 
-model_params = {0: {"scene_id": "chair_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+# model_params = {0: {"scene_id": "chair_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+# 					"ckpt_dir":"log_blender_hemisphere_new", "expname":"chair_linear_c128_i64", \
+# 					"mode":"linear", "color_mode":"midpoint", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+# 				1: {"scene_id": "chair_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+# 					"ckpt_dir":"log_blender_hemisphere_new", "expname":"chair_constant_c128_i64 ", \
+# 					"mode":"constant", "color_mode":"left", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5}																										
+# 				}
+
+model_params = {0: {"scene_id": "mic_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+					"ckpt_dir":"log_blender_hemisphere_new", "expname":"mic_linear_c128_i64", \
+					"mode":"linear", "color_mode":"midpoint", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+				1: {"scene_id": "mic_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+					"ckpt_dir":"log_blender_hemisphere_new", "expname":"mic_constant_c128_i64 ", \
+					"mode":"constant", "color_mode":"left", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+				2: {"scene_id": "lego_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+					"ckpt_dir":"log_blender_hemisphere_new", "expname":"lego_linear_c128_i64", \
+					"mode":"linear", "color_mode":"midpoint", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+				3: {"scene_id": "lego_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+					"ckpt_dir":"log_blender_hemisphere_new", "expname":"lego_constant_c128_i64_corrected ", \
+					"mode":"constant", "color_mode":"left", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+				4: {"scene_id": "hotdog_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+					"ckpt_dir":"log_blender_hemisphere_new", "expname":"hotdog_linear_c128_i64", \
+					"mode":"linear", "color_mode":"midpoint", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+				5: {"scene_id": "hotdog_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+					"ckpt_dir":"log_blender_hemisphere_new", "expname":"hotdog_constant_c128_i64 ", \
+					"mode":"constant", "color_mode":"left", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
+				6: {"scene_id": "chair_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
 					"ckpt_dir":"log_blender_hemisphere_new", "expname":"chair_linear_c128_i64", \
 					"mode":"linear", "color_mode":"midpoint", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5},
-				1: {"scene_id": "chair_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
+				7: {"scene_id": "chair_fixdist_nv100_dist0.5-1.5-5", "data_dir": "nerf_synthetic/fixed_dist_new/",\
 					"ckpt_dir":"log_blender_hemisphere_new", "expname":"chair_constant_c128_i64 ", \
-					"mode":"constant", "color_mode":"left", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5}																										
+					"mode":"constant", "color_mode":"left", "N_samples": 128, "N_importance": 64, "set_near_plane": 0.5}																															
 				}
 
 for model_idx in model_params:
